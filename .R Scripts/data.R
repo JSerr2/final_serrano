36cb7f4775ad944dafeba89cb13677962672d683
JSerr2 <Joseserranolansing@gmail.com>
2024-12-02 02:49:07 -0600

    loading, cleaning, and merging for new data set

--- .R Scripts/data.R
+++ .R Scripts/data.R
@@ -1,12 +1,19 @@
 library(tidyverse)
+library(tidycensus)
 
 # Importing original data sets, change directory to your saved file location
 
-attendance <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\24-RC-Pub-Data-Set.xlsx", sheet = "General")
+attendance <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\2019-Report-Card-Public-Data-Set.xlsx", sheet = "General")
 
-cep <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\fy24-eligibility.xlsx")
+cep <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\fy19-eligibilitydata.xlsx")
 
 # Data cleaning by eliminating spaces and creating consistent column names
+
+new_colnames <- as.character(cep[3, ])
+colnames(cep) <- new_colnames
+
+cep <- cep %>% 
+  slice(-1, -2, -3)
 
 colnames(cep) <- gsub(" ", "_", colnames(cep))
 colnames(attendance) <- gsub(" ", "_", colnames(attendance))
@@ -29,4 +36,34 @@ merged_data <- attendance %>%
 # Saved cleaned data, change directory to location where you want to save the data
 
 write_csv(merged_data, "C:/Users/joses/OneDrive/Documents/GitHub/final_serrano/Data/final_merged_data.csv")
+
+# Automatic Data Retrieval, input key in this line of code
+
+census_api_key("ae01069c564a94c61d198e88a6563d408de746a1", install = TRUE)
+
+# Elementary School Districts
+elementary_data <- get_acs(
+  geography = "school district (elementary)",
+  variables = c("B17020_002E"), 
+  state = "IL",
+  year = 2019
+)
+
+# Secondary School Districts
+secondary_data <- get_acs(
+  geography = "school district (secondary)",
+  variables = c("B17020_002E"),
+  state = "IL", 
+  year = 2019
+)
+
+# Unified School Districts
+unified_data <- get_acs(
+  geography = "school district (unified)",
+  variables = c("B17020_002E"),
+  state = "IL",
+  year = 2019
+)
+
+
 
