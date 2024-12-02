e914706ef82798996a89f1d182e68b13abef2b57
JSerr2 <Joseserranolansing@gmail.com>
2024-12-02 00:44:13 -0600

    Imported first two data sets
    Imported attendance records in Illinois data set and Illinois CEP data

---
+++ .R Scripts/data.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+# Importing original data sets, change directory to your saved file location
+
+attendance <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\24-RC-Pub-Data-Set.xlsx", sheet = "General")
+
+cep <- readxl::read_xlsx("C:\\Users\\joses\\OneDrive\\Documents\\GitHub\\final_serrano\\Data\\fy24-eligibility.xlsx")
+
+
+
