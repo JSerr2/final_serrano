afe7ebc0f234f1c843cae558dee014d83905e813
JSerr2 <Joseserranolansing@gmail.com>
2024-12-03 06:08:59 -0600

    name change in model

--- .R Scripts/model.R
+++ .R Scripts/model.R
@@ -1,4 +1,4 @@
-# Models
+# Model 1
 
 model_absenteeism <- lm(
   Chronic_Absenteeism ~ Participates_in_CEP * Poverty_Level + `%_Student_Enrollment_-_Low_Income` + Title_1_Binary,
@@ -6,6 +6,8 @@ model_absenteeism <- lm(
 )
 
 summary(model_absenteeism)
+
+# Model 2
 
 model_truancy <- lm(
   Student_Chronic_Truancy_Rate ~ Participates_in_CEP * Poverty_Level + `%_Student_Enrollment_-_Low_Income` + Title_1_Binary,
